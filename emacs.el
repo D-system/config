5f3d6a24cd5511f3e72a5f7af0438a1c592fc90a
Thomas Brennetot <thomas.brennetot@gmail.com>
2014-05-08 15:00:38 +0200

    set ruby-mode to Gemfile, .rake, etc

--- emacs.el
+++ emacs.el
@@ -53,5 +53,10 @@
 (load "~/config/emacs-mode/ruby-mode.el")
 (autoload 'ruby-mode "ruby-mode")
 (add-to-list 'auto-mode-alist '("\\.rb\\'" . ruby-mode))
-
+(add-to-list 'auto-mode-alist '("\\.rake$" . ruby-mode))
+(add-to-list 'auto-mode-alist '("Rakefile$" . ruby-mode))
+(add-to-list 'auto-mode-alist '("\\.gemspec$" . ruby-mode))
+(add-to-list 'auto-mode-alist '("\\.ru$" . ruby-mode))
+(add-to-list 'auto-mode-alist '("Gemfile$" . ruby-mode))
+(add-to-list 'auto-mode-alist '("Guardfile$" . ruby-mode))
 
