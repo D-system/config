887c47ceec6e2149461b203512eb2ca0e2f755ba
Thomas Brennetot <thomas@brennetot.com>
2025-01-01 20:58:54 +0100

    Convert to bash, use Mise and optimization

---
+++ install.sh
@@ -0,0 +1,79 @@
+CURRENT_DIR=$( dirname -- "$( readlink -f -- "$0"; )" );
+
+# Download CSM Breeze (git shortcut and utils)
+git clone git@github.com:scmbreeze/scm_breeze.git ~/.scm_breeze && ~/.scm_breeze/install.sh
+
+# Install Homebrew
+/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+
+# Install command line tools
+brew install the_silver_searcher ripgrep fd starship jq eza bat fzf
+
+brew install --cask \
+    google-chrome \
+    visual-studio-code \
+    docker \
+    slack \
+    wezterm \
+    meetingbar \
+    monitorcontrol \
+    keycastr \
+    shottr \
+    xnapper \
+    hiddenbar \
+    dash@6 \
+    # flameshot \
+
+
+# Install XCode
+xcode-select --install
+
+cp $CURRENT_DIR/zshrc.sh ~/.zshrc
+cp $CURRENT_DIR/gitconfig ~/.gitconfig
+cp $CURRENT_DIR/gemrc ~/.gemrc
+cp $CURRENT_DIR/ssh_config ~/.ssh/config
+
+curl https://mise.run | sh
+source ~/.zshrc
+mise use -g python ruby node
+gem update --system
+
+
+
+# Change MacOs defautls
+# Use `defaults read` to display main defaults settings.
+# See `man defaults` for the command
+
+# Change how fast the keyboard repeat keys
+defaults write NSGlobalDomain KeyRepeat -int 2
+defaults write com.apple.Accessibility KeyRepeatDelay "0.5"
+defaults write com.apple.Accessibility KeyRepeatInterval "0.083333333"
+
+# Trackpad "tap to click"
+defaults write com.apple.driver.AppleBluetoothMultitouch.trackpad Clicking -bool true
+defaults write com.apple.AppleMultitouchTrackpad Clicking -bool true
+
+# Remove margins on Windows
+defaults write com.apple.WindowManager EnableTiledWindowMargins -bool false
+
+# Disable Desktop/Space rearrangement
+defaults write com.apple.dock "mru-spaces" -bool false && killall Dock
+
+# Activate settings without logout
+/System/Library/PrivateFrameworks/SystemAdministration.framework/Resources/activateSettings -u
+
+
+echo "##########################"
+echo "## To download manually ##"
+echo "##########################"
+echo "https://apps.apple.com/jp/app/presentify-screen-annotation/id1507246666?l=en-US&mt=12"
+echo "https://apps.apple.com/jp/app/skitch-snap-mark-up-share/id425955336?l=en-US&mt=12"
+echo "https://apps.apple.com/jp/app/picture-in-picture/id6475380719?l=en-US&mt=12"
+
+
+echo ""
+echo ""
+echo "##########################"
+echo "##  To change manually  ##"
+echo "##########################"
+echo "Enable web inspector in Safari"
--- zshrc.sh
+++ zshrc.sh
@@ -1,139 +1,51 @@
-autoload -Uz promptinit
-promptinit
-
-setopt prompt_subst
-setopt interactivecomments
-autoload -Uz vcs_info
-zstyle ':vcs_info:*' stagedstr 'M'
-zstyle ':vcs_info:*' unstagedstr 'M'
-zstyle ':vcs_info:*' check-for-changes true
-zstyle ':vcs_info:*' actionformats '%F{5}[%F{2}%b%F{3}|%F{1}%a%F{5}]%f '
-zstyle ':vcs_info:*' formats \
-  '%F{5}[%F{2}%b%F{5}] %F{2}%c%F{3}%u%f'
-zstyle ':vcs_info:git*+set-message:*' hooks git-untracked
-zstyle ':vcs_info:*' enable git
-+vi-git-untracked() {
-  if [[ $(git rev-parse --is-inside-work-tree 2> /dev/null) == 'true' ]] && \
-  [[ $(git ls-files --other --directory --exclude-standard | sed q | wc -l | tr -d ' ') == 1 ]] ; then
-  hook_com[unstaged]+='%F{1}??%f'
-fi
-}
-
-precmd () { vcs_info }
-PROMPT='[%T] %F{3}%3~ ${vcs_info_msg_0_} %f%# '
-
-setopt histignorealldups sharehistory
-
-# Use emacs keybindings even if our EDITOR is set to vi
-bindkey -e
-
-# export EDITOR="sublime -w"
-export EDITOR="code --wait"
-
-# Lines configured by zsh-newuser-install
-HISTFILE=~/.histfile
-HISTSIZE=1000
-SAVEHIST=1000
-setopt appendhistory autocd extendedglob nomatch notify
-unsetopt beep
-# End of lines configured by zsh-newuser-install
-# The following lines were added by compinstall
-zstyle :compinstall filename '~/.zshrc'
-
+# compinit: helper for auto-completion
 autoload -Uz compinit
 compinit
 
-zstyle ':completion:*' auto-description 'specify: %d'
-zstyle ':completion:*' completer _expand _complete _correct _approximate
-zstyle ':completion:*' format 'Completing %d'
-zstyle ':completion:*' group-name ''
-zstyle ':completion:*' menu select=2
-zstyle ':completion:*:default' list-colors ${(s.:.)LS_COLORS}
-zstyle ':completion:*' list-colors ''
-zstyle ':completion:*' list-prompt %SAt %p: Hit TAB for more, or the character to insert%s
-zstyle ':completion:*' matcher-list '' 'm:{a-z}={A-Z}' 'm:{a-zA-Z}={A-Za-z}' 'r:|[._-]=* r:|=* l:|=*'
-zstyle ':completion:*' menu select=long
-zstyle ':completion:*' select-prompt %SScrolling active: current selection at %p%s
-zstyle ':completion:*' use-compctl false
-zstyle ':completion:*' verbose true
-zstyle ':completion:*:*:kill:*:processes' list-colors '=(#b) #([0-9]#)*=0=01;31'
-zstyle ':completion:*:kill:*' command 'ps -u $USER -o pid,%cpu,tty,cputime,cmd'
+eval "$(starship init zsh)"
+eval "$($HOME/.local/bin/mise activate zsh)"
 
-# End of lines added by compinstall
-
-alias ls='ls -h'
-if [[ `uname` = "Linux" ]]; then
-    # Linux
-    alias ls='ls --color=auto'
-else
-    # Unix
-    alias ls="ls -G"
-fi
-
+# Load SCM Breeze first because it conflict with something when outside of a git project
 [ -s "$HOME/.scm_breeze/scm_breeze.sh" ] && source "$HOME/.scm_breeze/scm_breeze.sh"
 
+alias ls='ls -G'
 alias l='ls -lh'
 alias la='l -a'
-alias dir='dir --color=auto'
-alias vdir='vdir --color=auto'
+alias ls="eza --git"
 
-alias grep='grep --color=auto'
-alias fgrep='fgrep --color=auto'
-alias egrep='egrep --color=auto'
-alias clean='find . -name "*~" -exec rm {} \; ; find . -name ".directory"  -exec rm {} \;'
-alias tree="ls -R | grep ':$' | sed -e 's/:$//' -e 's/[^\/]*\//|  /g' -e 's/|  \([^|]\)/\`--\1/g'"
+alias rg='rg -i'
+
+alias cat="bat --plain"
+
 alias glog="git log --graph --pretty=format:'%Cred%h%Creset -%C(yellow)%d%Creset %s %Cgreen(%cr) %C(bold blue)<%an>%Creset' --abbrev-commit"
 alias gitlog='glog'
+alias gpo='git push -u origin "$(git symbolic-ref --short HEAD)"'
+alias gsu='git branch --set-upstream-to=origin/`git symbolic-ref --short HEAD`'
+
+
+alias tree="ls -R | grep ':$' | sed -e 's/:$//' -e 's/[^\/]*\//|  /g' -e 's/|  \([^|]\)/\`--\1/g'"
 alias clone_site='wget --mirror --convert-links --backup-converted --no-clobber --adjust-extension --wait=5 --limit-rate=20k' # shorter equivalent: 'wget -m -k -K -nc -E -w --limit-rate=20k'
+
+alias grep='grep --color=auto -i'
 alias be='bundle exec'
 alias r='rails'
-alias rr='rake routes'
-alias berr='bundle exec rake routes'
-alias s="subl"
 alias c="code"
 
-export PATH=$PATH:$HOME/.rvm/bin # Add RVM to PATH for scripting
-export PATH=$PATH:/opt/local/bin # Macport directory
-export PATH="$PATH:/Applications/Sublime Text.app/Contents/SharedSupport/bin"
-export PATH="$PATH:/Applications/Visual Studio Code.app/Contents/Resources/app/bin"
-export PATH="$HOME/.rbenv/bin:$PATH"
-eval "$(rbenv init -)"
 
-export PKG_CONFIG_PATH=/usr/local/Cellar/imagemagick\@6/6.9.10-11/lib/pkgconfig/
-export PATH="/usr/local/opt/imagemagick@6/bin:$PATH"
-export PATH="/usr/local/opt/libxml2/bin:$PATH"
-export PATH="/usr/local/opt/mysql@5.7/bin:$PATH"
-export LIBRARY_PATH=$LIBRARY_PATH:/usr/local/opt/openssl/lib/
-
-# For localhost dev
-export PATH="$PATH:$HOME/projects/flutter/bin"
-export PATH="/usr/local/opt/libxml2/bin:$PATH"
-export PATH="./bin:$PATH"
-export PATH="$HOME/go/bin/:$PATH"
-
-export LDFLAGS="-L/usr/local/opt/libxml2/lib"
-export CPPFLAGS="-I/usr/local/opt/libxml2/include"
-
-export PKG_CONFIG_PATH="/usr/local/opt/libxml2/lib/pkgconfig"
-
-export PATH="/usr/local/opt/postgresql@10/bin:$PATH"
-if command -v pyenv 1>/dev/null 2>&1; then
-  eval "$(pyenv init -)"
-fi
-
-export PATH="$(pyenv root)/shims:$PATH"
-export NVM_DIR="$HOME/.nvm"
-[ -s "/usr/local/opt/nvm/nvm.sh" ] && . "/usr/local/opt/nvm/nvm.sh"
-[ -s "/usr/local/opt/nvm/etc/bash_completion.d/nvm" ] && . "/usr/local/opt/nvm/etc/bash_completion.d/nvm"
-
-export PATH="$PATH:$HOME/flutter/bin"
-export PATH="/usr/local/opt/openjdk/bin:$PATH"
-export PATH="$HOME/bin:$PATH"
-
-go_test() {
-  go test $* | sed ''/PASS/s//$(printf "\033[32mPASS\033[0m")/'' | sed ''/SKIP/s//$(printf "\033[34mSKIP\033[0m")/'' | sed ''/FAIL/s//$(printf "\033[31mFAIL\033[0m")/'' | sed ''/FAIL/s//$(printf "\033[31mFAIL\033[0m")/'' | GREP_COLOR="01;33" egrep --color=always '\s*[a-zA-Z0-9\-_.]+[:][0-9]+[:]|^'
+serve() {
+  ruby -run -ehttpd -- --port $1
 }
 
-[ -f ~/.fzf.zsh ] && source ~/.fzf.zsh
+export PATH="$HOME/.cargo/bin:${PATH}"
 
-eval "$(starship init zsh)"
+
+export EDITOR="code --wait"
+
+
+export RUBY_YJIT_ENABLE=true
+
+export FZF_COMPLETION_TRIGGER=''
+source <(fzf --zsh)
+bindkey '^T' fzf-completion
+bindkey '^I' $fzf_default_completion
+
