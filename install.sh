8b8a90a5cd38551b8656c554d2841d3b267a0212
Thomas Brennetot <thomas@brennetot.com>
2025-06-18 16:32:19 +0200

    Get Homebrew env setup in the script

--- install.sh
+++ install.sh
@@ -2,6 +2,7 @@ CURRENT_DIR=$( dirname -- "$( readlink -f -- "$0"; )" );
 
 # Install Homebrew
 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+eval "$(/opt/homebrew/bin/brew shellenv)"
 
 # Download CSM Breeze (git shortcut and utils)
 git clone git@github.com:scmbreeze/scm_breeze.git ~/.scm_breeze && ~/.scm_breeze/install.sh
